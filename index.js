68462ff194149f00342b97a820451a0bf4ad1594
pavandeep.devstree <pavandeep.devstree@gmail.com>
2024-08-21 20:22:19 +0530

    deployment issue fixed

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const app = express();
 app.use(express.json());
 PORT = process.env.PORT || 8080;
 MongoDBConnection()
+app.get('/', (req, res) => {
+  return res.json({
+    success: true,
+    data: "success"
+  })
+})
 app.use('/api/v1/user', userRouter)
 app.use('/api/v1/product', productRouter)
 app.listen(PORT, () => console.log(`Port is running at ${PORT}`))
 
-module.exports={app};
+module.exports = app
