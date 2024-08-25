3f841f9167ce89ff01496d6b1a7035cb52de7dcd
pavandeep.devstree <pavandeep.devstree@gmail.com>
2024-08-26 00:10:48 +0530

    remove function of redis

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,8 @@ const { productRouter } = require("../product");
 const { userRouter } = require("../user");
 
 function initialize(app) {
-    // app.use(corsMiddleware);
     app.use('/api/v1/user', userRouter);
-    app.use('/api/v1/product',authenticateJWT, productRouter); // Add authenticateJWT middleware to the product route
+    app.use('/api/v1/product',authenticateJWT, productRouter);
 }
 module.exports = {
     initialize
