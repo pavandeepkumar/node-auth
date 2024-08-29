9651143c20fc1ee22cc4e16d322a0d48630225d5
pavandeep.devstree <pavandeep.devstree@gmail.com>
2024-08-29 14:48:15 +0530

    add jwt

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const productRouter = require('../product/product.routes');
 const roleManagementRouter = require("../role-management/role-management.routes");
 function initialize(app) {
     app.use('/api/v1/user', userRouter);
-    app.use('/api/v1/product', productRouter);
+    app.use('/api/v1/product', authenticateJWT, productRouter);
     app.use('/api/v1/role', roleManagementRouter);
 }
 module.exports = {
